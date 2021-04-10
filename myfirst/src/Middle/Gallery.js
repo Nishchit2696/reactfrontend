d0c0e667e45ac2c4db37fac9fbdf21ab48a56196
Nishchit2696 <renish.shah8@gmail.com>
2021-04-10 14:23:10 +0545

    gallery

---
+++ myfirst/src/Middle/Gallery.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+class Gallery extends Component{
+    render(){
+        return(
+            <div className="card col-12 col-lg-4 login-card mt-2 hv-center" >
+               
+            </div>
+        )
+    }
+}
+export default Gallery;
