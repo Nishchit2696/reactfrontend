60a71e03dbaabf296513b3452da57bd579b9ee2c
Nishchit2696 <renish.shah8@gmail.com>
2021-04-10 14:21:36 +0545

    header

---
+++ myfirst/src/Footer/Footer.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import Map from './Map'
+
+class Footer extends Component{
+    render(){
+        return(
+            <div className="Footerbg">
+                <Map></Map>
+                </div>
+        )
+    }
+
+}
+
+export default Footer
